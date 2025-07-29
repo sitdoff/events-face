c329b28074bece14fd17ad999b1331a0017d4b79
Roman Ivanov <sitdoff@gmail.com>
2025-07-29 19:11:13 +0300

    add sync app

--- src/core/settings.py
+++ src/core/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     # Applications
     "src.events.apps.EventsConfig",
+    "src.sync.apps.SyncConfig",
     # Tools
     "debug_toolbar",
 ]
---
+++ src/sync/models/__init__.py
@@ -0,0 +1,1 @@
+from src.sync.models.synclog import SyncLogModel
