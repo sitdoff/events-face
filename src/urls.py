e016e3284d5cfe0ce51debb63e72c47621348b9b
Roman Ivanov <sitdoff@gmail.com>
2025-07-29 09:01:17 +0300

    add user register

--- src/urls.py
+++ src/urls.py
@@ -21,11 +21,14 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from src.users import RegisterView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("src.events.urls")),
     path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/register", RegisterView.as_view(), name="register"),
 ]
 
 if settings.DEBUG:
---
+++ src/users/__init__.py
@@ -0,0 +1,1 @@
+from src.users.views import RegisterView
