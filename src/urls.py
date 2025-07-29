4a0d400442576fdd6a0b7001344c4d729a419f56
Roman Ivanov <sitdoff@gmail.com>
2025-07-29 08:47:57 +0300

    update events.url

--- src/urls.py
+++ src/urls.py
@@ -24,8 +24,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("src.events.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 if settings.DEBUG:
