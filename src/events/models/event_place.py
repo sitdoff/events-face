93ad4fa02ec97dc1d2870f8cb20535c0be8bb0b8
Roman Ivanov <sitdoff@gmail.com>
2025-07-29 06:10:41 +0300

    update EventPlaceModel

--- src/events/models/event_place.py
+++ src/events/models/event_place.py
@@ -6,3 +6,10 @@ from django.db import models
 class EventPlaceModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255, verbose_name="Название места проведения")
+
+    class Meta:
+        verbose_name = "Место проведения мероприятия"
+        verbose_name_plural = "Места проведения мероприятий"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
