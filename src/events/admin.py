741dc57a17f18c823e7856732a4185a435c6e97e
Roman Ivanov <sitdoff@gmail.com>
2025-07-29 06:12:04 +0300

    add admin models

--- src/events/admin.py
+++ src/events/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from src.events.models import EventModel, EventPlaceModel
+
 # Register your models here.
+
+
+@admin.register(EventModel)
+class EventAdmin(admin.ModelAdmin):
+    verbose_name = "Мероприятие"
+    verbose_name_plural = "Мероприятия"
+    list_display = ["title", "date", "status", "place"]
+    list_filter = ["status", "place"]
+    search_fields = ["title", "place__title"]
+
+
+@admin.register(EventPlaceModel)
+class EventPlaceAdmin(admin.ModelAdmin):
+    verbose_name = "Место проведения мероприятия"
+    verbose_name_plural = "Места проведения мероприятия"
+    list_display = ["title"]
