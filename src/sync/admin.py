7214d617a89a077806ec7b61879706eb7a08bbc7
Roman Ivanov <sitdoff@gmail.com>
2025-07-30 14:26:59 +0300

    add SyncLogAdmin

--- src/sync/admin.py
+++ src/sync/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from src.sync.models import SyncLogModel
+
+
 # Register your models here.
+@admin.register(SyncLogModel)
+class SyncLogAdmin(admin.ModelAdmin):
+    list_display = ["run_date", "new_count", "updated_count"]
