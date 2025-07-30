db4f7ecf6092847e07e8bcac70059383d65963ec
Roman Ivanov <sitdoff@gmail.com>
2025-07-30 18:33:41 +0300

    update evens service and repo

--- src/events/repositories/event_repository.py
+++ src/events/repositories/event_repository.py
@@ -11,9 +11,13 @@ class EventRepository:
     model = EventModel
 
     def get_all(self) -> QuerySet[EventModel]:
-        queryset = self.model.objects.select_related("place").all()
+        queryset = self.model.objects.all()
         return queryset
 
     def get_all_open_events(self) -> QuerySet[EventModel]:
-        queryset = self.get_all().filter(status=EventModel.Status.OPEN)
+        queryset = self.get_all()
+        queryset = queryset.filter(status=EventModel.Status.OPEN)
         return queryset
+
+    def join_event_place(self, queryset: QuerySet[EventModel]) -> QuerySet[EventModel]:
+        return queryset.select_related("place")
--- src/events/services/event_service.py
+++ src/events/services/event_service.py
@@ -23,23 +23,12 @@ class EventService:
         queryset = self.event_repository.get_all()
         return queryset
 
-    def filter(self, queryset, **kwargs) -> QuerySet[EventModel]:
-        """
-        Фильтрация EventModel
-        """
-        return queryset.filter(**kwargs)
-
-    def ordering(self, queryset, order_by: str) -> QuerySet[EventModel]:
-        """
-        Сортировка по указаному полю
-        """
-        return queryset.order_by(order_by)
-
     def get_events(self, request: Request, **kwargs) -> QuerySet[EventModel]:
         """
         Получает EventModel с фильтрацией по полю имени
         """
         queryset = self.event_repository.get_all_open_events()
+        queryset = self.event_repository.join_event_place(queryset)
         if (name := request.query_params.get("name")) is not None:
-            queryset = self.filter(queryset, name__icontains=name)
+            queryset = queryset.filter(name__icontains=name)
         return queryset
